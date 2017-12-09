298ce3b9906fac4fac9a4892daf4a3fa114fbbd9
Lwfrancisco <lwfrancisco@gmail.com>
2017-12-08 19:17:35 -0700

    First revision with ASCII Table
    Signed-off-by: Logan Francisco <lwfrancisco@gmail.com>

--- main.py
+++ main.py
@@ -8,6 +8,7 @@ from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
 
 import kivy
+
 kivy.require('1.10.0')
 
 
@@ -20,20 +21,36 @@ class RootWidget(BoxLayout):
 
 
 class CalculatorApp(App):
-
     '''This is the app itself'''
 
     def build(self):
         '''This method loads the root.kv file automatically
 
-        :rtype: none
+        :return: none
         '''
         # loading the content of root.kv
         self.root = Builder.load_file('kv_modes/root.kv')
+
+    def screen_select(self, selector):
+        '''
+
+        :param selector: character that is dependant on which mode button is toggled.
+        :return:
+        '''
+
+        filename = selector + '.kv'
+        # unload the content of the .kv file
+        # reason: may have data from previous calls
+        Builder.unload_file('kv_modes/' + filename)
+        # clear the container
+        self.root.container.clear_widgets()
+        # load the .kv file
+        selector = Builder.load_file('kv_modes/' + filename)
+        # add content of the .kv file to the container
+        self.root.container.add_widget(selector)
 
 
 if __name__ == '__main__':
     '''Start the application'''
 
     CalculatorApp().run()
-
