5d51a6f3e8ceb5f5b901bddc7c7aec98658e2280
Lwfrancisco <lwfrancisco@gmail.com>
2017-12-08 22:54:47 -0700

    Last before due date
    Signed-off-by: Logan Francisco <lwfrancisco@gmail.com>

--- main.py
+++ main.py
@@ -6,10 +6,24 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.boxlayout import BoxLayout
 from kivy.properties import ObjectProperty
+from kivy.uix.gridlayout import GridLayout
 
 import kivy
 
 kivy.require('1.10.0')
+
+
+class CalcGridLayout(GridLayout):
+
+    # Function called when equals is pressed
+    def calculate(self, calculation):
+        if calculation:
+            try:
+                # Solve formula and display it in entry
+                # which is pointed at by display
+                self.display.text = str(eval(calculation))
+            except Exception:
+                self.display.text = "Error"
 
 
 class RootWidget(BoxLayout):
