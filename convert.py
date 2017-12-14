70a38c3f56443d198c69de6570e7810e763bb171
YenHungLai <34381523+YenHungLai@users.noreply.github.com>
2017-12-14 09:23:59 -0700

    Add files via upload

---
+++ convert.py
@@ -0,0 +1,38 @@
+    def num_conversion(self, input1, output, data):
+        try:
+            if input1 == 'Decimal':
+                if output == 'Decimal':
+                    self.display.text = str(int(data))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data)))
+                elif output == 'octal':
+                    self.display.text = str(oct(int(data)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data)))
+            if input1 == 'Hexadecimal':
+                if output == 'Decimal':
+                    self.display.text = str(int(data, 16))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data, 16)))
+                elif output == 'Octal':
+                    self.display.text = str(oct(int(data, 16)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data, 16)))
+            if input1 == 'Octal':
+                if output == 'Decimal':
+                    self.display.text = str(int(data, 8))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data, 8)))
+                elif output == 'Octal':
+                    self.display.text = str(oct(int(data, 8)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data, 8)))
+            if input1 == 'Binary':
+                if output == 'Decimal':
+                    self.display.text = str(int(data, 2))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data, 2)))
+                elif output == 'Octal':
+                    self.display.text = str(oct(int(data, 2)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data, 2)))
