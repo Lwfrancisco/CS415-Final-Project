ca7dbcd50054a4fbdd4bb59da0dd8c1e7b47bc0e
Lwfrancisco <lwfrancisco@gmail.com>
2017-12-14 19:59:31 -0700

    Final Commit before turning in. Messed with base font sizes.
    Signed-off-by: Logan Francisco <lwfrancisco@gmail.com>

--- main.py
+++ main.py
@@ -19,10 +19,13 @@ class CalcGridLayout(GridLayout):
     # Function called when equals is pressed
     def calculate(self, calculation):
         if calculation:
+
+            string = calculation
+
             try:
                 # Solve formula and display it in entry
                 # which is pointed at by display
-                self.display.text = str(eval(calculation))
+                self.display.text = str(eval(string.replace('&&', 'and').replace('||', 'or').replace('!', 'not ')))
             except Exception:
                 self.display.text = "Error"
 
@@ -37,9 +40,44 @@ class RootWidget(BoxLayout):
 
 class NumConv(BoxLayout):
 
-    def num_conversion(self, input, output, data):
+    def num_conversion(self, input1, output, data):
         try:
-            self.display.text = data
+            if input1 == 'Decimal':
+                if output == 'Decimal':
+                    self.display.text = str(int(data))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data)))
+                elif output == 'Octal':
+                    self.display.text = str(oct(int(data)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data)))
+            if input1 == 'Hexadecimal':
+                if output == 'Decimal':
+                    self.display.text = str(int(data, 16))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data, 16)))
+                elif output == 'Octal':
+                    self.display.text = str(oct(int(data, 16)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data, 16)))
+            if input1 == 'Octal':
+                if output == 'Decimal':
+                    self.display.text = str(int(data, 8))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data, 8)))
+                elif output == 'Octal':
+                    self.display.text = str(oct(int(data, 8)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data, 8)))
+            if input1 == 'Binary':
+                if output == 'Decimal':
+                    self.display.text = str(int(data, 2))
+                elif output == 'Hexadecimal':
+                    self.display.text = str(hex(int(data, 2)))
+                elif output == 'Octal':
+                    self.display.text = str(oct(int(data, 2)))
+                elif output == 'Binary':
+                    self.display.text = str(bin(int(data, 2)))
         except Exception:
             self.display.text = "Error"
 
